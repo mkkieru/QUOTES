909e90664afab9b79f20481ae60252c59a6f43e6
mkkieru <mkieru55@gmail.com>
2021-07-19 10:45:41 +0300

    Change quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,12 +10,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quote: Quote[] = [
-    new Quote(1, 'I think the people in this country have had enough of experts with organisations from acronyms saying that they know what is best and getting it consistently wrong.', 'Benitha','Janet', new Date(2021,3,14),0,0),
-    new Quote(2,'Always end your book with Nelson Mandela saying something about rainbows or renaissances. Because you care.','Ange','Naomy',new Date(2021,5,9),0,0),
-    new Quote(3,'How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesnt make it a leg.','Diana','Tina', new Date(2020,4,12),0,0),
-    new Quote(4,'Having a positive mental attitude is asking how something can be done rather than saying it cant be done.','Angelic','Tharcissie',new Date(2021,3,18),0,0),
-    new Quote(5,'Stop saying these negative things about yourself. Look in the mirror and find something about yourself thats positive and celebrate that!','Ganza','Lyse', new Date(2021,2,14),0,0),
-    new Quote(6,'They invented hugs to let people know you love them without saying anything.','Adeline','Divine',new Date(2021,1,14),0,0),
+    new Quote(1, 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson','Mandela', new Date(2021,3,14),0,0),
+    new Quote(2,'The way to get started is to quit talking and begin doing. ','Walt','Disney',new Date(2021,5,9),0,0),
+    new Quote(3,'Your time is limited, so do not waste it living someone elses life. Do not be trapped by dogma – which is living with the results of other peoples thinking.','Steve','Jobs', new Date(2020,4,12),0,0),
+    new Quote(4,'If life were predictable it would cease to be life, and be without flavor.','Eleanor','Roosevelt',new Date(2021,3,18),0,0),
+    new Quote(5,'If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough.','Oprah','Winfrey', new Date(2021,2,14),0,0),
   ];
 
   toggleDetails(index:any){
